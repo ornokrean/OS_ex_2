760bb68cfc2b30354803ad4ca6e4472aa3e4a880
erezuni <erez.hecht@mail.huji.ac.il>
2019-04-08 16:15:00 +0300

    thread class

---
+++ thread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by erezuni on 4/8/19.
+//
+
+#include "thread.h"
---
+++ thread.h
@@ -0,0 +1,16 @@
+//
+// Created by erezuni on 4/8/19.
+//
+
+#ifndef EX2_THREAD_H
+#define EX2_THREAD_H
+
+
+class thread
+{
+private:
+public:
+};
+
+
+#endif //EX2_THREAD_H
